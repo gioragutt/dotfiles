6bf3d059251fae7ab4283372b8f1267da3635eea
Giora Guttsait <giora111@gmail.com>
2022-09-13 09:01:16 +0000

    add initial stuff

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
---
+++ zsh_aliases.sh
@@ -0,0 +1,6 @@
+alias gis="git status"
+alias gif="git diff"
+alias gig="git graph"
+alias doc="docker-compose"
+alias repos="cd ~/Git/"
+alias cdgit='cd $(git rev-parse --show-toplevel)'
