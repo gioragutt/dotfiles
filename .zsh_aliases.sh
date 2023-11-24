c12d574085775eb9104621e4ac455cf9c9b09c25
Giora Guttsait <giora111@gmail.com>
2023-11-24 15:14:45 +0200

    Update .zsh_aliases.sh

--- .zsh_aliases.sh
+++ .zsh_aliases.sh
@@ -1,6 +1,20 @@
 alias gis="git status"
 alias gif="git diff"
 alias gig="git graph"
-alias doc="docker-compose"
+alias gigr="git graph-raw"
+alias doc="docker compose"
 alias repos="cd ~/Git/"
-alias cdgit='cd $(git rev-parse --show-toplevel)'
+alias cdgit="cd $(git rev-parse --show-toplevel)"
+alias c="clear"
+alias p="pnpm"
+alias nx="npx nx"
+alias code="code-insiders"
+
+# Config Files
+
+alias reload_aliases="source ~/.zsh_aliases"
+alias reload_zshrc="source ~/.zshrc"
+
+alias edit_alises="code ~/.zsh_aliases"
+alias edit_zshrc="code ~/.zshrc"
+alias edit_gitconfig="code ~/.gitconfig"
